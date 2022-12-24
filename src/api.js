569d3391e5800095fdcad1ec8da0abcf073dd1fa
vishnupriyagithub <gandhalavishnup@gmail.com>
2022-12-24 12:03:05 +0530

    modified

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6ea1339289msh321199d48a7033ap1a99dajsnf220d4180719',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f25269994e8f89ce7c5f0abe80d3fe28";
